4dc3ae7a6e5a2cea74270fd9823076b7a6f720dd
LojainHassan <lojainhteabat@gmail.com>
2023-11-10 19:26:21 +0300

    Explain pronpt in js

--- AhmadDamra/wwwroot/js/site.js
+++ AhmadDamra/wwwroot/js/site.js
@@ -2,3 +2,17 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+
+function promptInteract() {
+    var uName = prompt("Enter your usserame :", "Lojain");
+    var passWord = prompt("Enter your password :");
+    if (uName == "admin" && passWord == "123") {
+        document.write("Welcome Admin");
+    }
+    else {
+        document.write("Wrong pasword / userName");
+        var uName = prompt("Enter your usserame :", "Lojain");
+        var passWord = prompt("Enter your password :");
+    }
+}
